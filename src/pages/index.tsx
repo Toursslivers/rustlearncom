22d9a27bf8c86f336c724c28a53e27f32c1f30f8
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-01-30 10:10:39 +0800

    feat: add clsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,8 +15,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+        </div>
+      </header>
       <main>
-        <div className="text-2xl leading-6">123</div>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+          </div>
+          {/* /End replace */}
+        </div>
       </main>
     </>
   )
